3f29f10f89006a8b215b860a8ae8985499bf3854
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-09 16:31:55 +0800

    完成UI

--- build-JetBrainsActivation-Desktop_Qt_6_4_2_MinGW_64_bit-Debug/JetBrainsActivation_autogen/include/ui_jetbrainsactivation.h
+++ build-JetBrainsActivation-Desktop_Qt_6_4_2_MinGW_64_bit-Debug/JetBrainsActivation_autogen/include/ui_jetbrainsactivation.h
@@ -14,6 +14,7 @@
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -23,7 +24,16 @@ class Ui_JetBrainsActivation
 {
 public:
     QWidget *centralwidget;
+    QLabel *Title;
+    QPushButton *CLion;
+    QPushButton *DataGrip;
+    QPushButton *GoLand;
+    QPushButton *IDEA;
+    QPushButton *PhpStorm;
+    QPushButton *PyCharm;
+    QPushButton *WebStorm;
     QLabel *label;
+    QLabel *label_2;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -31,19 +41,46 @@ public:
     {
         if (JetBrainsActivation->objectName().isEmpty())
             JetBrainsActivation->setObjectName("JetBrainsActivation");
-        JetBrainsActivation->resize(280, 362);
+        JetBrainsActivation->resize(311, 230);
         centralwidget = new QWidget(JetBrainsActivation);
         centralwidget->setObjectName("centralwidget");
-        label = new QLabel(centralwidget);
-        label->setObjectName("label");
-        label->setGeometry(QRect(50, 0, 211, 21));
+        Title = new QLabel(centralwidget);
+        Title->setObjectName("Title");
+        Title->setGeometry(QRect(60, 0, 211, 21));
         QFont font;
         font.setPointSize(14);
-        label->setFont(font);
+        Title->setFont(font);
+        CLion = new QPushButton(centralwidget);
+        CLion->setObjectName("CLion");
+        CLion->setGeometry(QRect(10, 30, 93, 29));
+        DataGrip = new QPushButton(centralwidget);
+        DataGrip->setObjectName("DataGrip");
+        DataGrip->setGeometry(QRect(110, 30, 93, 29));
+        GoLand = new QPushButton(centralwidget);
+        GoLand->setObjectName("GoLand");
+        GoLand->setGeometry(QRect(210, 30, 93, 29));
+        IDEA = new QPushButton(centralwidget);
+        IDEA->setObjectName("IDEA");
+        IDEA->setGeometry(QRect(10, 70, 93, 29));
+        PhpStorm = new QPushButton(centralwidget);
+        PhpStorm->setObjectName("PhpStorm");
+        PhpStorm->setGeometry(QRect(110, 70, 93, 29));
+        PyCharm = new QPushButton(centralwidget);
+        PyCharm->setObjectName("PyCharm");
+        PyCharm->setGeometry(QRect(210, 70, 93, 29));
+        WebStorm = new QPushButton(centralwidget);
+        WebStorm->setObjectName("WebStorm");
+        WebStorm->setGeometry(QRect(10, 110, 93, 29));
+        label = new QLabel(centralwidget);
+        label->setObjectName("label");
+        label->setGeometry(QRect(10, 140, 261, 20));
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(10, 160, 291, 20));
         JetBrainsActivation->setCentralWidget(centralwidget);
         menubar = new QMenuBar(JetBrainsActivation);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 280, 26));
+        menubar->setGeometry(QRect(0, 0, 311, 26));
         JetBrainsActivation->setMenuBar(menubar);
         statusbar = new QStatusBar(JetBrainsActivation);
         statusbar->setObjectName("statusbar");
@@ -57,7 +94,16 @@ public:
     void retranslateUi(QMainWindow *JetBrainsActivation)
     {
         JetBrainsActivation->setWindowTitle(QCoreApplication::translate("JetBrainsActivation", "JetBrainsActivation", nullptr));
-        label->setText(QCoreApplication::translate("JetBrainsActivation", "JetBrainsActivation", nullptr));
+        Title->setText(QCoreApplication::translate("JetBrainsActivation", "JetBrainsActivation", nullptr));
+        CLion->setText(QCoreApplication::translate("JetBrainsActivation", "CLion", nullptr));
+        DataGrip->setText(QCoreApplication::translate("JetBrainsActivation", "DataGrip", nullptr));
+        GoLand->setText(QCoreApplication::translate("JetBrainsActivation", "GoLand", nullptr));
+        IDEA->setText(QCoreApplication::translate("JetBrainsActivation", "IDEA", nullptr));
+        PhpStorm->setText(QCoreApplication::translate("JetBrainsActivation", "PhpStorm", nullptr));
+        PyCharm->setText(QCoreApplication::translate("JetBrainsActivation", "PyCharm", nullptr));
+        WebStorm->setText(QCoreApplication::translate("JetBrainsActivation", "WebStorm", nullptr));
+        label->setText(QCoreApplication::translate("JetBrainsActivation", "Warning:If you download these app", nullptr));
+        label_2->setText(QCoreApplication::translate("JetBrainsActivation", "from toolbox, it can't activate correctly.", nullptr));
     } // retranslateUi
 
 };

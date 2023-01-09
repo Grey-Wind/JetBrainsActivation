a547d25969254ee0ba69e8cdb9ceb2a3dc723e6b
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-01-09 16:19:04 +0800

    修改UI

--- build-JetBrainsActivation-Desktop_Qt_6_4_2_MinGW_64_bit-Debug/JetBrainsActivation_autogen/include/ui_jetbrainsactivation.h
+++ build-JetBrainsActivation-Desktop_Qt_6_4_2_MinGW_64_bit-Debug/JetBrainsActivation_autogen/include/ui_jetbrainsactivation.h
@@ -11,6 +11,7 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
@@ -22,6 +23,7 @@ class Ui_JetBrainsActivation
 {
 public:
     QWidget *centralwidget;
+    QLabel *label;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -29,12 +31,19 @@ public:
     {
         if (JetBrainsActivation->objectName().isEmpty())
             JetBrainsActivation->setObjectName("JetBrainsActivation");
-        JetBrainsActivation->resize(800, 600);
+        JetBrainsActivation->resize(280, 362);
         centralwidget = new QWidget(JetBrainsActivation);
         centralwidget->setObjectName("centralwidget");
+        label = new QLabel(centralwidget);
+        label->setObjectName("label");
+        label->setGeometry(QRect(50, 0, 211, 21));
+        QFont font;
+        font.setPointSize(14);
+        label->setFont(font);
         JetBrainsActivation->setCentralWidget(centralwidget);
         menubar = new QMenuBar(JetBrainsActivation);
         menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 280, 26));
         JetBrainsActivation->setMenuBar(menubar);
         statusbar = new QStatusBar(JetBrainsActivation);
         statusbar->setObjectName("statusbar");
@@ -48,6 +57,7 @@ public:
     void retranslateUi(QMainWindow *JetBrainsActivation)
     {
         JetBrainsActivation->setWindowTitle(QCoreApplication::translate("JetBrainsActivation", "JetBrainsActivation", nullptr));
+        label->setText(QCoreApplication::translate("JetBrainsActivation", "JetBrainsActivation", nullptr));
     } // retranslateUi
 
 };
